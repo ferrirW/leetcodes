bfcbd47f95e7f6f50ef41d7bb2f9c654815ea251
wangfan <wangfan8@xiaomi.com>
2020-12-14 22:28:31 +0800

    update

--- coding/src/main/java/PredictPartyVictory.java
+++ coding/src/main/java/PredictPartyVictory.java
@@ -1,11 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class PredictPartyVictory {
 
     private static String str = "RD";
     private static String str2 = "RDD";
 
-    // TODO
     public String predictPartyVictory(String senate) {
+        Queue<Integer> R = new LinkedList<>();
+        Queue<Integer> D = new LinkedList<>();
+        for (int i = 0; i < senate.length(); i++) {
+            if (senate.charAt(i) == 'R') {
+                R.offer(i);
+            } else {
+                D.offer(i);
+            }
+        }
+        while (!R.isEmpty() && !D.isEmpty()) {
+            int r = R.poll();
+            int d = D.poll();
+            if (r < d) {
+                R.offer(r+senate.length());
+            } else {
+                D.offer(d+senate.length());
+            }
+        }
 
+        if (R.isEmpty()) {
+            return "Dire";
+        } else {
+            return "Radiant";
+        }
     }
 
     public static void main(String[] args) {
